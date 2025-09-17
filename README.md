aa5781cf8bdfc2148be80adfdd80ab3606b61d5d
mcasarin <casarin.marcio@gmail.com>
2025-09-17 16:48:29 -0300

    atualizando projeto para entrega

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 ## Projeto de avaliação para o curso de lógica de Programação ##
 Professor Felipe Viana, DIO (Digital Innovation One)
 
-Foram apresentados conceitos e exemplos de lógica de programação utilizando Javascript, segue ementa:
+Foram apresentados conceitos e exemplos de lógica de programação utilizando Javascript, Playcode.io e W3Schools Compiler e VSCode. Segue ementa:
 
     - Algoritmos e Fluxograma
     - Variáveis
@@ -15,3 +15,25 @@ Foram apresentados conceitos e exemplos de lógica de programação utilizando J
     - Git, Github (utilizado deploy por ssh-key)
 
 ## Executei o curso como forma de atualizar conceitos e ter uma visão diferenciada, __Long Learning Life__ (softskill)
+
+## Desafio ##
+**O que dever ser utilizado**
+- Variáveis
+- Operadores
+- Laços de repetição
+- Estruturas de decisões
+
+## Objetivo ##
+Crie uma variável para armazenar o nome e a quantidade de experiência (XP) de um herói, depois utilize uma estrutura de decisão para apresentar alguma das mensagens abaixo:
+Se XP for menor do que 1000 = Ferro
+Se XP for entre 1001 e 2000 = Bronze
+Se XP for entre 2001 e 5000 = Prata
+Se XP for entre 5001 e 7000 = Ouro
+Se XP for entre 7001 e 8000 = Platina
+Se XP for entre 8001 e 9000 = Ascendente
+Se XP for entre 9001 e 10000 = Imortal
+Se XP for maior ou igual a 10001 = Radiante
+
+### Saída ###
+Ao final deve ser exibida uma mensagem:
+"O herói de nome **{nome}** está no nível de **{nível}**"
--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-console.log("Firt file");
+console.log("Validando se o herói evoluiu de nível");
+
+let heroi = {
+    nome: "Gandalf",
+    nivel: 9900,
+    classe: "Mago",
+    vida: 1000,
+    mana: 5000
+}
+
+let txtHeroi = "O herói de nome "
+let txtNivel = " está no nível de "
+
+switch (true) { // usando para o caso específico do desafio para retornar texto
+    case heroi.nivel <= 1000:
+        console.log( txtHeroi + heroi.nome + txtNivel +"Ferro.");
+        break;
+    case heroi.nivel >= 1001 && heroi.nivel <= 2000:
+        console.log( txtHeroi + heroi.nome + txtNivel +"Bronze.");
+        break;
+    case heroi.nivel >= 2001 && heroi.nivel <= 5000:
+        console.log( txtHeroi + heroi.nome + txtNivel +"Prata.");
+        break;
+    case heroi.nivel >= 5001 && heroi.nivel <= 7000:
+        console.log( txtHeroi + heroi.nome + txtNivel +"Ouro.");
+        break;
+    case heroi.nivel >= 7001 && heroi.nivel <= 8000:
+        console.log( txtHeroi + heroi.nome + txtNivel +"Platina.");
+        break;
+    case heroi.nivel >= 8001 && heroi.nivel <= 9000:
+        console.log( txtHeroi + heroi.nome + txtNivel +"Ascendente.");
+        break;
+    case heroi.nivel >= 9001 && heroi.nivel <= 10000:
+        console.log( txtHeroi + heroi.nome + txtNivel +"Imortal.");
+        break;
+    case heroi.nivel >= 10001:
+        console.log( txtHeroi + heroi.nome + txtNivel +"Radiante.");
+        break;
+    default:
+        console.log( txtHeroi + heroi.nome + txtNivel +"que eu não consegui calcular!.");
+}
